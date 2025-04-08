167c6a83a81b856a849812a85506ba23e97e8415
MaryKhm656 <nanoman66656@gmail.com>
2025-04-08 15:32:24 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-from fastapi.openapi.models import Operation
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 from datetime import datetime
@@ -12,22 +11,47 @@ class BankAccount(Base):
     owner = Column(String, nullable=False)
     balance = Column(Float, default=0.0)
     created_at = Column(DateTime, default=datetime.utcnow)
+    _pin = Column("pin", String, nullable=False)
     
     operations = relationship("Operation", back_populates="account", cascade="all, delete-orphan")
     
-    def deposit(self, amount):
+    def set_pin(self, pin: str):
+        self._pin = pin
+        
+    def check_pin(self, pin: str):
+        return self._pin == pin
+    
+    def deposit(self, amount, pin):
+        if not self.check_pin(pin):
+            raise ValueError("Неверный PIN-код")
         if amount <= 0:
             raise ValueError("Сумма должна быть положительной")
         self.balance += amount
         self.add_operation("Пополнение", amount)
         
-    def withdraw(self, amount):
+    def withdraw(self, amount, pin):
+        if not self.check_pin(pin):
+            raise ValueError("Неверный PIN-код")
         if amount <= 0:
             raise ValueError("Сумма должна быть положительной")
         if amount < self.balance:
             raise ValueError("Недостаточно средств")
         self.balance -= amount
         self.add_operation("Снятие", amount)
+
+    def transfer(self, to_account, amount, pin):
+        if not self.check_pin(pin):
+            raise ValueError("Неверный PIN-код")
+        if amount <= 0:
+            raise ValueError("Сумма должна быть положительной")
+        if amount > self.balance:
+            raise ValueError("Недостаточно средств")
+
+        self.balance -= amount
+        to_account.balance += amount
+
+        self.add_operation(f"Перевод на аккаунт {to_account.id}", -amount)
+        to_account.add_operation(f"Перевод от аккаунта {self.id}", amount)
         
     def add_operation(self, type_, amount):
         op = Operation(type=type_, amount=amount, timestamp=datetime.utcnow(), account=self)
