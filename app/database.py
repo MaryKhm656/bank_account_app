a34749fd0ad66511e1c9f0e7642231890b8d7fc5
MaryKhm656 <nanoman66656@gmail.com>
2025-04-08 12:24:02 +0300

    Update database.py

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+engine = create_engine("sqlite:///bank.db", echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
